51fd565f2731d3dc8e40ffe9336d40626821b3fe
Juho Teperi <juho.teperi@iki.fi>
2015-05-15 10:44:59 +0300

    0.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/palikka "0.1.0"
+(defproject metosin/palikka "0.1.0-SNAPSHOT"
   :description "Metosin palikka"
   :url "https://github.com/metosin/palikka"
   :license {:name "Eclipse Public License"
@@ -6,8 +6,8 @@
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [prismatic/schema "0.4.0"]
-                 [metosin/maailma "0.1.0"]
+                 [prismatic/schema "0.4.2"]
+                 [metosin/maailma "0.1.0-SNAPSHOT"]
                  [com.stuartsierra/component "0.2.3"]]
   :plugins [[codox "0.8.10"]]
 
