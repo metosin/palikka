4e0cd9e6d777db1ff9d112fe96a3c808095afe7f
Juho Teperi <juho.teperi@iki.fi>
2015-06-25 18:35:58 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                 [prismatic/schema "0.4.2"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [prismatic/schema "0.4.3"]
                  [metosin/maailma "0.1.0-SNAPSHOT"]
-                 [metosin/schema-tools "0.4.2"]
+                 [metosin/schema-tools "0.4.3"]
                  [com.stuartsierra/component "0.2.3"]]
   :plugins [[codox "0.8.10"]]
 
@@ -21,10 +21,10 @@
                                   [clj-http-lite "0.2.1"]
 
                                   ; Components
-                                  [com.novemberain/monger "2.0.1"]
-                                  [hikari-cp "1.2.1"]
-                                  [http-kit "2.1.18"]
-                                  [clj-http "1.1.0"]
+                                  [com.novemberain/monger "2.1.0"]
+                                  [hikari-cp "1.2.4"]
+                                  [http-kit "2.1.19"]
+                                  [clj-http "1.1.2"]
                                   [hikari-cp "1.2.1"]
                                   [postgresql/postgresql "9.3-1102.jdbc41"]
                                   [org.flywaydb/flyway-core "3.2.1"]]}
