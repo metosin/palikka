4ba307a307a5a1500bf6219bd678c5421097b685
Juho Teperi <juho.teperi@iki.fi>
2015-06-25 20:43:00 +0300

    notes

--- test/palikka/example/system.clj
+++ test/palikka/example/system.clj
@@ -9,13 +9,19 @@
 (defn base-system [config]
   (component/system-map
     :env         (-> (env/create "example" config)
+                     ; provides spec: using vector
                      (providing [:config]))
     :db          (-> (db/create)
                      (using [:env])
+                     ; injections spec: using function
+                     ; Note: references the system / component directly, maybe
+                     ; should go through provides?
                      (injecting {:config #(-> % :env :config :db)})
+                     ; provides spec: map
                      (providing {:db :db, :gfs :gfs}))
     :handler     (-> (handler/create 'palikka.example.handler/app)
                      (using [:env :db]))
     :http-kit    (-> (http-kit/create)
                      (using [:env :handler])
+                     ; injections spec: using keyword vector
                      (injecting {:config [:env :config :http]}))))
