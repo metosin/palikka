194154bcd6a18cd47c09d86c33b84c2e4c19e0a6
Juho Teperi <juho.teperi@iki.fi>
2015-09-15 13:47:19 +0300

    Clear test namespaces

--- test/palikka/core_test.clj
+++ test/palikka/core_test.clj
@@ -1,9 +1,6 @@
 (ns palikka.core-test
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
             [com.stuartsierra.component :as component :refer [system-map using]]
-            [palikka.components.env :as env]
-            [palikka.components.handler :as handler]
-            [palikka.components.http-kit :as http-kit]
             [palikka.core :refer :all]
             [palikka.test-utils :refer :all]
             [clj-http.client :as client]
@@ -12,15 +9,16 @@
 (defonce system (base-system {:http {:port 9999}, :test "hello world" :db {:foo :bar}}))
 (use-fixtures :once (system-fixture #'system))
 
+(defn ctx [] (create-context system))
+
 (deftest system-test
   (is (some? (get-in system [:http-kit :http-kit])))
   (is (= "hello world"
          (:body (client/get "http://localhost:9999")))))
 
 (deftest context-test
-  (let [ctx (create-context system)]
-    (is (map? (:config ctx)))
-    (is (instance? clojure.lang.Atom (:db ctx)))))
+  (is (map? (:config (ctx))))
+  (is (instance? clojure.lang.Atom (:db (ctx)))))
 
 (comment
   (system-up! #'system)
--- test/palikka/midje_test.clj
+++ test/palikka/midje_test.clj
@@ -1,9 +1,6 @@
 (ns palikka.midje-test
   (:require [midje.sweet :refer :all]
             [com.stuartsierra.component :as component :refer [system-map using]]
-            [palikka.components.env :as env]
-            [palikka.components.handler :as handler]
-            [palikka.components.http-kit :as http-kit]
             [palikka.core :refer [create-context]]
             [palikka.test-utils :refer :all]
             [clj-http.client :as client]
@@ -12,6 +9,8 @@
 (defonce system (base-system {:http {:port 9999} :test "hello world"}))
 (background (around :facts (system-background #'system ?form)))
 
+(defn ctx [] (create-context system))
+
 (facts system-test
   (fact "foo"
     (get-in system [:http-kit :http-kit])) => truthy
@@ -19,9 +18,8 @@
     (:body (client/get "http://localhost:9999")) => "hello world"))
 
 (facts context
-  (let [ctx (create-context system)]
-    (:config ctx) => map?
-    (instance? clojure.lang.Atom (:db ctx)) => truthy))
+  (:config (ctx)) => map?
+  (instance? clojure.lang.Atom (:db (ctx))) => truthy)
 
 (facts system-test-2
   (let [db (:db (:db system))]
