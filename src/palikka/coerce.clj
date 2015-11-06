e66250001fbef17f0efc0a87d1abfae6ad5a580b
Juho Teperi <juho.teperi@iki.fi>
2015-11-07 00:29:51 +0200

    Add string to collection coercion matcher

--- src/palikka/coerce.clj
+++ src/palikka/coerce.clj
@@ -1,9 +1,29 @@
 (ns palikka.coerce
+  "Provides Schema coercion for component configuration values.
+
+  This is useful when options are read from string only sources like
+  envinronment variables or system properties. Coercion is based on
+  schema.coerce/string-coercion-matcher but additionally includes
+  following coercions:
+
+  - Single string to collection by splitting the string on \":\""
   (:require [schema.coerce :as sc]
-            [schema.utils :as su]))
+            [schema.utils :as su]
+            [clojure.string :as string]))
+
+(defn string-collection-matcher [schema]
+  (if (and (coll? schema) (not (map? schema)))
+    (fn [v]
+      (if (string? v)
+        (into (empty schema) (string/split v #":"))
+        v))))
+
+(defn env-coercion-matcher [schema]
+  (or (sc/string-coercion-matcher schema)
+      (string-collection-matcher schema)))
 
 (defn env-coercer [schema]
-  (sc/coercer schema sc/+string-coercions+))
+  (sc/coercer schema env-coercion-matcher))
 
 (defn env-coerce
   "Coerces and validates value against given schema. Throws if
--- test/palikka/coerce_test.clj
+++ test/palikka/coerce_test.clj
@@ -4,10 +4,14 @@
             [palikka.coerce :refer :all]))
 
 (s/defschema Config {(s/optional-key :port) s/Int
-                     (s/optional-key :ssl?) s/Bool})
+                     (s/optional-key :ssl?) s/Bool
+                     (s/optional-key :schemas) [s/Str]
+                     (s/optional-key :ports) [s/Int]})
 
 (deftest env-coerce-test
   (is (= {:port 3000} (env-coerce Config {:port "3000"})))
   (is (= {:ssl? true} (env-coerce Config {:ssl? "true"})))
+  (is (= {:schemas ["flyway" "app"]} (env-coerce Config {:schemas "flyway:app"})))
+  (is (= {:ports [123 234]} (env-coerce Config {:ports "123:234"})))
   (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Component options validation failed"
                         (env-coerce Config {:port :3000}))))
