cf010ead9e90a2479df6e7144bc7a3e2f76fd4e7
Juho Teperi <juho.teperi@iki.fi>
2015-11-07 00:21:00 +0200

    Add rabbitmq config schema

--- src/palikka/components/rabbitmq.clj
+++ src/palikka/components/rabbitmq.clj
@@ -2,8 +2,19 @@
   (:require [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
             [schema.core :as s]
-            [langohr.core :as rmq])
+            [langohr.core :as rmq]
+            [palikka.coerce :as c])
   (:import [com.rabbitmq.client Connection]))
+
+(s/defschema Config
+  {(s/optional-key :host) s/Str
+   (s/optional-key :port) s/Int
+   (s/optional-key :username) s/Str
+   (s/optional-key :password) s/Str
+   (s/optional-key :vhost) s/Str
+   (s/optional-key :ssl) s/Bool
+   (s/optional-key :uri) s/Str
+   s/Keyword s/Str})
 
 (defrecord RabbitMQ [^Connection mq config]
   component/Lifecycle
@@ -17,4 +28,4 @@
     (assoc this :mq nil)))
 
 (defn create [config]
-  (map->RabbitMQ {:config config}))
+  (map->RabbitMQ {:config (c/env-coerce Config config)}))
