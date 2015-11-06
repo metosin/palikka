f71db4aeddfc05ce30f1b9366713d4203706446f
Juho Teperi <juho.teperi@iki.fi>
2015-11-06 23:56:40 +0200

    Bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/palikka "0.2.1-SNAPSHOT"
+(defproject metosin/palikka "0.2.2-SNAPSHOT"
   :description "Metosin palikka"
   :url "https://github.com/metosin/palikka"
   :license {:name "Eclipse Public License"
