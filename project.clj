02b441de4ff58fe7801dac6a83483a166269e7c5
Juho Teperi <juho.teperi@iki.fi>
2016-04-13 14:05:50 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[potemkin "0.4.1"]
+  :dependencies [[potemkin "0.4.3"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [prismatic/schema "1.0.3"]
-                 [metosin/schema-tools "0.6.2"]
-                 [com.stuartsierra/component "0.3.0"]]
+                 [prismatic/schema "1.1.0"]
+                 [metosin/schema-tools "0.9.0"]
+                 [com.stuartsierra/component "0.3.1"]]
   :plugins [[lein-codox "0.9.0"]]
 
   :codox {:source-uri "http://github.com/metosin/palikka/blob/master/{filepath}#L{line}"}
@@ -18,26 +18,26 @@
                              [lein-midje "3.2"]]
                    :source-paths ["examples"]
                    :resource-paths ["test-resources"]
-                   :dependencies [[org.clojure/clojure "1.7.0"]
-                                  [midje "1.8.1"]
-                                  [clj-http "2.0.0"]
+                   :dependencies [[org.clojure/clojure "1.8.0"]
+                                  [midje "1.8.3"]
+                                  [clj-http "3.0.0"]
                                   [reloaded.repl "0.2.1"]
-                                  [metosin/maailma "0.2.0-SNAPSHOT"]
+                                  [metosin/maailma "0.2.0"]
 
                                   ; Components
-                                  [com.novemberain/monger "3.0.1"]
+                                  [com.novemberain/monger "3.0.2"]
                                   [hikari-cp "1.3.1"]
                                   [http-kit "2.1.19"]
                                   [postgresql/postgresql "9.3-1102.jdbc41"]
-                                  [org.flywaydb/flyway-core "3.2.1"]
+                                  [org.flywaydb/flyway-core "4.0"]
                                   [org.clojure/tools.nrepl "0.2.12"]
-                                  [com.datomic/datomic-free "0.9.5327" :exclusions [org.slf4j/slf4j-nop joda-time]]
-                                  [com.novemberain/langohr "3.4.1"]
-                                  [aleph "0.4.1-beta2"]
+                                  [com.datomic/datomic-free "0.9.5350" :exclusions [org.slf4j/slf4j-nop joda-time]]
+                                  [com.novemberain/langohr "3.5.1"]
+                                  [aleph "0.4.1"]
 
                                   ; Logging
-                                  [ch.qos.logback/logback-classic "1.1.3"]
-                                  [org.slf4j/slf4j-api "1.7.12"]
+                                  [ch.qos.logback/logback-classic "1.1.7"]
+                                  [org.slf4j/slf4j-api "1.7.21"]
                                   ]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0-beta2"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.8"]
